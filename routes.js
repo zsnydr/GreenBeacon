33d3d90fcaf48b90e230d77cecd92873438bd830
Conor <oleary.conor17@gmail.com>
2016-10-14 09:28:24 -0700

    [FIX] make route functions work

--- routes.js
+++ routes.js
@@ -1,23 +1,23 @@
-var helpers = require('./helpers');
+// var helpers = require('./authHelpers');
 var passport = require('passport');
 var helpers = require('./routehelpers');
 module.exports.router = function(app){
 
 app.get('/signin', helpers.authfunction);
 app.get('/signout', helpers.terminateSession)
-app.get('/auth/github',
-    passport.authenticate('github', { scope: [ 'user:email' ] }),
-    function(req, res){
-      // The request will be redirected to GitHub for authentication, so this
-      // function will not be called.
-    });
-
-app.get('/callback',
-    passport.authenticate('github', { failureRedirect: '/session' }),
-    (req, res) => {
-      console.log('SESSION:', req.session);
-      res.redirect('/');
-    });
+// app.get('/auth/github',
+//     passport.authenticate('github', { scope: [ 'user:email' ] }),
+//     function(req, res){
+//       // The request will be redirected to GitHub for authentication, so this
+//       // function will not be called.
+//     });
+//
+// app.get('/callback',
+//     passport.authenticate('github', { failureRedirect: '/session' }),
+//     (req, res) => {
+//       console.log('SESSION:', req.session);
+//       res.redirect('/');
+//     });
 app.get('/tickets', helpers.getTicketsFunc);
 
 
