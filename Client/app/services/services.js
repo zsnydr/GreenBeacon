1bae5c677893e8f2dac014f14122c3e9b88f2469
natasha-t <nthapli@gmail.com>
2016-10-13 20:02:25 -0700

    [add] setup auth signin

--- Client/app/services/services.js
+++ Client/app/services/services.js
@@ -1,2 +1,18 @@
 //declare 'Auth' as factory
   //pass in $http object for requests
+
+angular.module('app.services', [])
+
+.factory('Auth', ['$http', function($http){
+
+  var signin = function() {
+    return $http({
+      method: 'GET',
+      url: '/auth/github'
+    })
+    .then(function(resp){
+      return resp;
+    })
+  }
+
+}])
