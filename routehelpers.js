6f516b965d7c30a78eb475356b9bbfe638198234
zsnydr <zackwsnyder@gmail.com>
2016-10-14 14:59:16 -0700

    [ADD] add pseudocode to server helpers

--- routehelpers.js
+++ routehelpers.js
@@ -1,9 +1,11 @@
 
-var messages = [
-  { username: 'Natasha', message: 'HELP ME', location: 'Kitchen', ID: 1, date: new Date() },
-  { username: 'Alina', message: 'Help with backbone', location: 'Couch', ID: 2, date: new Date() },
-  { username: 'Conor', message: 'How do you react?', location: 'Pairing', ID: 3, date: new Date() },
-  { username: 'Zack', message: 'ugh, Grunt', location: 'Lecture Hall', ID: 4, date: new Date() }
+var IDcount = 1;
+
+var tickets = [
+  { username: 'Natasha', message: 'HELP ME', location: 'Kitchen', ID: 1, date: new Date(), claimed: false, solved: false },
+  { username: 'Alina', message: 'Help with backbone', location: 'Couch', ID: 2, date: new Date(), claimed: true, solved: false },
+  { username: 'Conor', message: 'How do you react?', location: 'Pairing', ID: 3, date: new Date(), claimed: false, solved: false },
+  { username: 'Zack', message: 'ugh, Grunt', location: 'Lecture Hall', ID: 4, date: new Date(), claimed: true, solved: true }
 ];
 
 module.exports = {
@@ -16,16 +18,23 @@ module.exports = {
     console.log('terminateSession');
   },
 
-  getTicketsFunc: function(){
+  getTicketsFunc: function(req, res){
     console.log('getTicketsFunc');
+    // send all tickets back in res.end
   },
 
-  addToQueue: function(){
-    console.log('conor')
+  addToQueue: function(req, res){
+    console.log('conor');
+    // pull username, message, and location from req.body and IDcount
+    // build ticket object
+    // push ticket to messages
+    // increment IDcount
   },
 
-  tagClaimed: function(){
+  tagClaimed: function(req, res){
     console.log('claimed');
+    // find ticket with messageID from req
+    // mark it as claimed
   },
 
   tagSolved: function(){
