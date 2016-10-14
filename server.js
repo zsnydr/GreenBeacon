91f012d99889d4d98f3a91ce4e65c48171197fa6
Conor <oleary.conor17@gmail.com>
2016-10-14 10:42:48 -0700

    [FIX] headers again

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ app.use(parser.json());
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type', 'X-HTTP-Method-Override', 'Accept');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, X-HTTP-Method-Override, Accept');
     res.setHeader('Access-Control-Allow-Credentials', true);
     next();
   });
