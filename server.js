77062bca4473958a752819647edd05ac6bb416d4
Conor <oleary.conor17@gmail.com>
2016-10-14 10:16:09 -0700

    [FIX] routes for real

--- server.js
+++ server.js
@@ -49,6 +49,13 @@ app.use(parser.json());
 
 // Serve the client files
 app.use(express.static(__dirname + '/Client'));
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+  });
 routes.router(app);
 authorize.auth(app);
 
