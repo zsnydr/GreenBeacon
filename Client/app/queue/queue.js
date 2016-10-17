97276af9cec06d49389d1536be8ae98dee9762a8
zsnydr <zackwsnyder@gmail.com>
2016-10-17 11:27:36 -0700

    [REFACTOR] Move all routes into routes.js and move helpers into routehelpers.js

--- Client/app/queue/queue.js
+++ Client/app/queue/queue.js
@@ -17,6 +17,6 @@ angular.module('app.queue', [])
       })
   }
 
-  initializeQueue();
+  //initializeQueue();
 
 }])
--- routes.js
+++ routes.js
@@ -1,32 +1,31 @@
 // var helpers = require('./authHelpers');
 var passport = require('passport');
 var helpers = require('./routehelpers');
-module.exports.router = function(app){
 
-  app.get('/signin', function(req, res){
-    res.redirect('/auth/github');
+module.exports.router = (app) => {
+
+  // app.get('/signin', (req, res) => {
+  //   res.redirect('/auth/github');
+  // });
+
+  app.get('/auth/github', passport.authenticate('github', { scope: [ 'user:email' ] }), (req, res) => {
+    // The request will be redirected to GitHub for authentication, so this
+    // function will not be called.
   });
-  app.get('/signout', helpers.terminateSession)
-  // app.get('/auth/github',
-  //     passport.authenticate('github', { scope: [ 'user:email' ] }),
-  //     function(req, res){
-  //       // The request will be redirected to GitHub for authentication, so this
-  //       // function will not be called.
-  //     });
-  //
-  // app.get('/callback',
-  //     passport.authenticate('github', { failureRedirect: '/session' }),
-  //     (req, res) => {
-  //       console.log('SESSION:', req.session);
-  //       res.redirect('/');
-  //     });
-  app.get('/tickets', helpers.getTicketsFunc(req, res));
 
+  app.get('/callback', passport.authenticate('github', { failureRedirect: '/session' }), (req, res) => {
+    console.log('SESSION:', req.session);
+    res.redirect('/');
+  });
 
-  app.post('/tickets', helpers.addToQueue(req, res));
+  app.get('/tickets', helpers.getTicketsFunc);
 
+  app.post('/tickets', helpers.addToQueue);
 
-  app.put('/claimed', helpers.tagClaimed(req, res));
+  app.put('/claimed', helpers.tagClaimed);
+
   app.put('/solved', helpers.tagSolved);
-  
-}
+
+  app.get('/signout', helpers.terminateSession)
+
+};
