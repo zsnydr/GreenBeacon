417f8b16523a3cd001dc0a57fa6464026282f6b2
natasha-t <nthapli@gmail.com>
2016-10-19 13:52:31 -0700

    [add] add database directory with schema.js, export models

---
+++ db/schema.js
@@ -0,0 +1,79 @@
+
+var pg = require('pg')
+var Sequelize = require('sequelize');
+
+
+var db = new Sequelize(process.env.DATABASE_URL, {
+ dialect: 'postgres'
+});
+
+db
+ .authenticate()
+ .then(function(err) {
+   console.log('Connection established');
+ })
+ .catch(function(err) {
+   console.log('Unable to connect: ', err);
+ });
+
+
+var User = db.define('user', {
+ id: {
+   type: Sequelize.INTEGER,
+   primaryKey: true,
+   autoIncrement: true
+ },
+ username: Sequelize.STRING
+});
+
+var Ticket = db.define('ticket', {
+ id: {
+   type: Sequelize.INTEGER,
+   primaryKey: true,
+   autoIncrement: true
+ },
+ message: Sequelize.TEXT,
+ location: Sequelize.STRING,
+ claimed: {
+   type: Sequelize.BOOLEAN,
+   defaultValue: false
+ },
+ solved: {
+   type: Sequelize.BOOLEAN,
+   defaultValue: false
+ }
+});
+
+var Claim = db.define('claim', {
+ id: {
+   type: Sequelize.INTEGER,
+   primaryKey: true,
+   autoIncrement: true
+ }
+});
+
+
+User.hasMany(Ticket);
+Ticket.belongsTo(User);
+
+User.hasMany(Claim);
+Claim.belongsTo(User);
+
+Ticket.hasOne(Claim);
+Claim.belongsTo(Ticket);
+
+
+db
+ .sync({force: true})
+ .then(function() {
+   console.log('Tables created');
+   return Ticket.create({message: 'Need help HELP', location: 'kitchen' }).then(function() {
+     console.log('Created new ticket');
+   });
+ });
+
+module.exports = {
+   User: User,
+   Ticket: Ticket,
+   Claim: Claim
+}
