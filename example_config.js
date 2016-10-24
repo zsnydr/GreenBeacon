d6f940ced81af330eca9bab448ddbaf1b4b48cc3
zsnydr <zackwsnyder@gmail.com>
2016-10-24 11:57:34 -0700

    [REFACTOR] quick cleanup

--- example_config.js
+++ example_config.js
@@ -4,5 +4,5 @@ if(process.env.NODE_ENV !== 'production') {
 	  gitHubClientId: 'Your Localhost Client ID',
 	  gitHubSecretKey: 'Your Localhost Secret Key',
 	  gitCallbackUrl: 'http://localhost:3000/callback'
-	}
+  }
 }
