a4bd47f1c5d626f0bdb3f90f387e80b3e2f17d4f
Alina <alina.lobastova.8@gmail.com>
2016-10-24 11:39:43 -0700

    [refactor] cleanup schema.js file

--- Client/app/auth/auth.js
+++ Client/app/auth/auth.js
@@ -3,7 +3,7 @@ angular.module('app.auth', [])
 .controller('AuthController', ['$scope', 'Auth', function ($scope, Auth) {
 //Signin function attached to scope
   $scope.signin = function() {
-  	//Triggers signin from Auth factory
+    //Triggers signin from Auth factory
     Auth.signin();
-  }
+  };
 }]);
--- db/schema.js
+++ db/schema.js
@@ -1,67 +1,71 @@
-
 var pg = require('pg')
 var Sequelize = require('sequelize');
-
-
 var db = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/beacon', {
- dialect: 'postgres'
+  dialect: 'postgres'
 });
 
+//Establishes the connection to the database
 db
- .authenticate()
- .then(function(err) {
-   console.log('Connection established');
- })
- .catch(function(err) {
-   console.log('Unable to connect: ', err);
- });
+  .authenticate()
+  .then(function (err) {
+    console.log('Connection established');
+  })
+  .catch(function (err) {
+    console.log('Unable to connect: ', err);
+  });
 
-
+//Creates table of users
 var User = db.define('user', {
- id: {
-   type: Sequelize.INTEGER,
-   primaryKey: true,
-   autoIncrement: true
- },
- username: Sequelize.STRING,
- displayname: Sequelize.STRING
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  username: Sequelize.STRING, //GitHub username
+  displayname: Sequelize.STRING //full first and last name
 });
 
+//Creates table of tickets
 var Ticket = db.define('ticket', {
- id: {
-   type: Sequelize.INTEGER,
-   primaryKey: true,
-   autoIncrement: true
- },
- message: Sequelize.TEXT,
- location: Sequelize.STRING,
- x: Sequelize.INTEGER,
- y: Sequelize.INTEGER,
- color: Sequelize.STRING,
- claimed: {
-   type: Sequelize.BOOLEAN,
-   defaultValue: false
- },
- solved: {
-   type: Sequelize.BOOLEAN,
-   defaultValue: false
- },
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  message: Sequelize.TEXT,
+  location: Sequelize.STRING,
+  //pulsing dot coordinates
+  x: Sequelize.INTEGER,
+  y: Sequelize.INTEGER,
+  //dot color
+  color: Sequelize.STRING,
+
+  claimed: {
+    type: Sequelize.BOOLEAN,
+    defaultValue: false
+  },
+  solved: {
+    type: Sequelize.BOOLEAN,
+    defaultValue: false
+  },
   preSolved: {
-   type: Sequelize.BOOLEAN,
-   defaultValue: false
- }
+    type: Sequelize.BOOLEAN,
+    defaultValue: false
+  }
 });
 
+//creates table of claimed tickets
 var Claim = db.define('claim', {
- id: {
-   type: Sequelize.INTEGER,
-   primaryKey: true,
-   autoIncrement: true
- },
- helpeeId: Sequelize.INTEGER
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  //id of the user who claimed the ticket
+  helpeeId: Sequelize.INTEGER
 });
 
-
+//Defines relationships between tables
 User.hasMany(Ticket);
 Ticket.belongsTo(User);
 
@@ -71,18 +75,15 @@ Claim.belongsTo(User);
 Ticket.hasOne(Claim);
 Claim.belongsTo(Ticket);
 
-
+//Create Tables
 db
- .sync({force: true})
- .then(function() {
-   console.log('Tables created');
-  //  return Ticket.create({message: 'Need help HELP', location: 'kitchen' }).then(function() {
-  //    console.log('Created new ticket');
-  //  });
+  .sync({force: true})
+  .then(function() {
+    console.log('Tables created');
  });
 
 module.exports = {
-   User: User,
-   Ticket: Ticket,
-   Claim: Claim
-}
+  User: User,
+  Ticket: Ticket,
+  Claim: Claim
+};
