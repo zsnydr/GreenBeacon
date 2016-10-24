cc36968b5204301d0ea72f8a4dd779581b01c7bd
Alina <alina.lobastova.8@gmail.com>
2016-10-24 11:22:02 -0700

    [refactor] cleanup html files

--- Client/app/auth/auth.js
+++ Client/app/auth/auth.js
@@ -1,9 +1,9 @@
 //Signin controller
 angular.module('app.auth', [])
-.controller('AuthController', ['$scope', 'Auth', function($scope, Auth) {
+.controller('AuthController', ['$scope', 'Auth', function ($scope, Auth) {
 //Signin function attached to scope
   $scope.signin = function() {
-  	//triggers signin from Auth factory
+  	//Triggers signin from Auth factory
     Auth.signin();
   }
 }]);
