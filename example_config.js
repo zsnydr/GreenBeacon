3d02d83102015a5611c0c9fc14d367606287d97b
Alina <alina.lobastova.8@gmail.com>
2016-10-25 08:08:59 -0700

    [refactor] remove APi keys

--- example_config.js
+++ example_config.js
@@ -1,8 +1,8 @@
 if(process.env.NODE_ENV !== 'production') {
   // i.e. you are working from localhost
   module.exports.keys = {
-    gitHubClientId: '2bf2f840356d251d928c',
-    gitHubSecretKey: 'c606d2126dd0ea186e3dda5d53f1646bf778cc10',
+    gitHubClientId: 'Your Localhost Client ID',
+    gitHubSecretKey: 'Your Localhost Secret Key',
     gitCallbackUrl: 'http://localhost:3000/callback'
   }
 }
